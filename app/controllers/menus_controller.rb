b1ddb19be484ceaaf300b24cc2321a1251c6a549
prithi-fw <preethi.haridas@freshworks.com>
2021-08-23 18:12:02 +0530

    display menu from each menu-category

--- app/controllers/menus_controller.rb
+++ app/controllers/menus_controller.rb
@@ -2,4 +2,9 @@ class MenusController < ApplicationController
     def index
       render plain: Menu.all.map { |menu| menu.to_pleasant_string }.join("\n")
     end
+    def show
+        id = params[:id]
+        menu_items = MenuItem.where("menu_id=?", id)
+        render plain: menu_items.all.map { |menu_item| menu_item.to_pleasant_string }.join("\n")
+      end
   end
--- app/models/menu_item.rb
+++ app/models/menu_item.rb
@@ -1,3 +1,6 @@
 class MenuItem < ActiveRecord::Base
     belongs_to :menu
+    def to_pleasant_string
+        "#{menu_item_id}.  #{menu_item_name}  #{menu_item_price}"
+      end
   end
