ff14ccbe342924700f77eb3a14c79735076ef449
prithi-fw <preethi.haridas@freshworks.com>
2021-08-24 14:56:24 +0530

    Created home page

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  
+# For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  get "/" => "home#index"
   resources :menu_items
   resources :menus
 end
